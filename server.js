c33e2055f5dfacbde65fcc438cde55d9d302f7b8
Littledropbootcamp <littledropedu@gmail.com>
2022-07-29 23:57:32 +0100

    carousel images updated

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 // Form manipulation
-var regBtn = document.querySelectorAll(".dropdown-item");
-var dropDiv = document.getElementsByClassName("hero");
-var login = document.getElementById('login');
-var register = document.getElementById('register')
-var button = document.getElementById('btn');
-console.log(login, register, button);
+// var regBtn = document.querySelectorAll(".dropdown-item");
+// var dropDiv = document.getElementsByClassName("hero");
+// var login = document.getElementById('login');
+// var register = document.getElementById('register')
+// var button = document.getElementById('btn');
+// console.log(login, register, button);
 
-function myRegister(){
-  login.style.left ="-400px";
-  register.style.left ="50px";
-  button.style.left ="110px";
-}
+// function myRegister(){
+//   login.style.left ="-400px";
+//   register.style.left ="50px";
+//   button.style.left ="110px";
+// }
 
-function myLogin(){
-  login.style.left ="50px";
-  register.style.left ="450px";
-  button.style.left ="0px";
-}
-myRegister()
-myLogin()
+// function myLogin(){
+//   login.style.left ="50px";
+//   register.style.left ="450px";
+//   button.style.left ="0px";
+// }
+// myRegister()
+// myLogin()
 
 
-//Donation tab
+//Donation tab===========================================================================
 function showDiv(){
   // on click the donation details will be displayed
   document.getElementById("show-div").style.display = "block";
@@ -37,76 +37,15 @@ document.getElementById("show-div").style.display ="none";
 
 hideDiv();
 
+//-------------------------Enroll now===============================
+//document.addEventListener("click", display)
+document.getElementById("action-btn").addEventListener("click", function(){alert("Hello World!");});
+
+// function myFunction() {
+//   document.getElementById("display-info").innerHTML = ();
+// }
 
 
-/*--------------------------------------------Search------------------------------------------------------*/
-const courses= [
-{name: 'business intelligence'},
-{name: 'data analysis'},
-{name: 'data science'},
-{name: 'digital marketing'},
-{name: 'employability skills'},
-{name: 'mobile developemen'},
-{name: 'project management'},
-{name: 'social media marketing'},
-{name: 'web development'}
-];
-
-const list = document.getElementById('list');
-
-function setList(group){
-clearList();
-for (const courses of group){
-const item =document.createElement('li');
-item.classList.add('list-group-item');
-const text = document.createTextNode(courses.name);
-item.appendChild(text);
-list.appendChild(item);
-}
-if (group.length === 0){
-  setNoResults();
-}
-}
-
-function clearList(){
-  while(list.firstChild){
-    list.removeChild(list.firstChild)
-  }
-}
-function setNoResults(){
-  const item =document.createElement('li');
-  item.classList.add('list-group-item');
-  const text = document.createTextNode('No results found');
-  item.appendChild(text);
-  list.appendChild(item);
-}
-
-function getRelevancy(value, searchTerm){
-  if(value === searchTerm){
-    return 2;
-  }else if (value.startWith(term)){
-    return 1;
-  }else{
-    return 0;
-  }
-}
-
-const searchInput =document.getElementById('search-input');
-
-searchInput.addEventListener('input', (event) =>{
-  let value = event.target.value;
-  if(value && value.trim().length > 0){
-    value = value.trim().toLowerCase();
-    setList(courses.filter(learn =>{
-      return learn.name.includes(value);
-
-    }).sort((learnA, learnB) =>{
-      return getRelevancy(learnB.name, value)- getRelevancy(learnA.name, value);
-    }));
-  }else{
-    clearList();
-  }
-})
 // show login form
 function myFunction() {
   var y = document.getElementById("clickme");
